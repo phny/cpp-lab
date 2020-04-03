99b0b85352a6b421f5366d24b23d041ba9124421
heyulin <heyulin@sensetime.com>
2020-04-03 21:42:00 +0800

    add: substr_test.cpp

---
+++ copy_test.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: ../copy_test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年04月03日 星期五 18时01分41秒
+ ************************************************************************/
+
+// copy algorithm example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::copy
+#include <vector>       // std::vector
+
+int main () {
+  std::string a = "skkkkkdd";
+  std::string b;
+  std::copy(a.begin(), a.end(), b.begin());
+  
+  std::cout << b << std::endl;
+
+  return 0;
+}
