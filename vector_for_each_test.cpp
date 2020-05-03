6c42555dbf59133a08cc28e47390e08c1e0cc0fc
hyl <1063766703@qq.com>
2020-05-03 09:13:59 +0800

    add: vector_for_each_test.cpp

---
+++ vector_for_each_test.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: vector_for_each_test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年04月22日 星期三 21时59分21秒
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+ 	std::vector<string> myvector;
+	myvector.push_back("a.txt");
+	myvector.push_back("b.txt");
+	myvector.push_back("c.txt");
+    
+    std::for_each (myvector.begin(), myvector.end(), [](string a) {
+        cout << a << endl;
+    });
+
+    std::replace_if(myvector.begin(), myvector.end(), [](string a) {
+
+    });
+
+    return 0;
+}
+
