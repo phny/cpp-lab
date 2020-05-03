afb50d2ebfa10dbb5db98eab97c1776a8f5b67ef
hyl <1063766703@qq.com>
2020-05-03 12:15:27 +0800

    add: class-template-static-var test

---
+++ class-template-static-var/main.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年05月03日 星期日 12时04分12秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+extern void call();
+extern void call_1();
+
+int main() {
+    call();
+    call_1();
+}
+
---
+++ class-template-static-var/the_class.hpp
@@ -0,0 +1,9 @@
+template <typename T>
+struct the_class {
+    static int id;
+    the_class() { ++id;  }
+};
+
+// 初始化静态变量
+template <typename T> 
+int the_class<T>::id = 0;
