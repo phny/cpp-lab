b5b1c685ad59b4a64b483e8193165b0ca7115d4e
hyl <1063766703@qq.com>
2020-06-07 23:18:05 +0800

    add: test-cases/async_test.cpp

--- test-cases/lambda_test_2.cpp
+++ test-cases/lambda_test_2.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
-        > File Name: lambda_test_2.cpp
-        > Author:
-        > Mail:
-        > Created Time: 2020年04月24日 星期五 14时08分36秒
- ************************************************************************/
+> File Name: lambda_test_2.cpp
+> Author:
+> Mail:
+> Created Time: 2020年04月24日 星期五 14时08分36秒
+************************************************************************/
 
 #include <iostream>
 #include <vector>
@@ -11,18 +11,19 @@
 using namespace std;
 
 int main() {
-  int nums = 10;
-  std::vector<int> ret = [nums] {
-    std::vector<int> v;
-    v.resize(10);
-    for (int i = 0; i < nums; i++) {
-      v[i] = i * i;
-    }
-    return v;
-  }();
+    // 通过 lambda 表达式来初始化 vector
+    int nums = 10;
+    std::vector<int> ret = [nums] {
+        std::vector<int> v;
+        v.resize(10);
+        for (int i = 0; i < nums; i++) {
+            v[i] = i * i;
+        }
+        return v;
+    }();
 
-  for (auto &i : ret) {
-    cout << i << endl;
-  }
-  return 0;
+    for (auto &i : ret) {
+        cout << i << endl;
+    }
+    return 0;
 }
