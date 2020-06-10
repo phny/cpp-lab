a0ab279fc6da449677361c4eff5f9be4392786fe
hyl <1063766703@qq.com>
2020-06-10 23:51:13 +0800

    stream iterator read write file

--- test-cases/istream_iterator_read_file_test.cpp
+++ test-cases/istream_iterator_read_file_test.cpp
@@ -9,15 +9,18 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <string>
+
+using namespace std;
 
 int main() {
   std::ifstream dataFile("data.txt");
-  std::istream_iterator<int> dataBegin(dataFile);
-  std::istream_iterator<int> dataEnd;
-  std::vector<int> data(dataBegin, dataEnd);
+  std::istream_iterator<string> dataBegin(dataFile);
+  std::istream_iterator<string> dataEnd;
+  std::vector<string> data(dataBegin, dataEnd);
 
-  for (int i : data) {
-    std::cout << i << "\t";
+  for (auto& i : data) {
+      std::cout << i << std::endl;
   }
   std::cout << std::endl;
 
