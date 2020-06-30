ffa6a0842db83aab6f1f102374ec1ec64ee694b8
hyl <1063766703@qq.com>
2020-07-01 07:51:26 +0800

    add: gtest-demo

---
+++ gtest-demo/build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../lib/libsrc.a"
+  "../lib/libsrc.pdb"
+  "CMakeFiles/src.dir/calc.cpp.o"
+  "CMakeFiles/src.dir/main.cpp.o"
+  "CMakeFiles/src.dir/string_util.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gtest-demo/src/string_util.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: string_util.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年07月01日 星期三 07时38分49秒
+ ************************************************************************/
+
+# include "stdlib.h"
+# include "string_util.h"
+int StringUtil::ToInt(const char * str)
+{
+    return atol(str);
+}
