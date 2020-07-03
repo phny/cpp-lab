c0ee04d760f935bbf70123f0a2f55d193a034086
heyulin <heyulin@sensetime.com>
2020-07-03 23:11:27 +0800

    add: pybind11-cases/py2cpp.cpp

---
+++ pybind11-cases/py2cpp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <pybind11/pybind11.h>
+
+using namespace std;
+namespace py = pybind11;
+
+int add(int i, int j) { return i + j; }
+
+float add(float f1, float f2) { return f1 + f2; }
+
+double add(double a, double b) { return a + b; }
+
+class Hello {
+public:
+  Hello() {}
+  void say(const std::string s) { std::cout << s << std::endl; }
+};
+
+PYBIND11_MODULE(py2cpp, m) {
+  m.doc() = "pybind11 example";
+  m.def("add", static_cast<int (*)(int, int)>(&add), "add two int number");
+  m.def("add", static_cast<float (*)(float, float)>(&add),
+        "add two float number");
+  m.def("add", [](const double a, const double b) {});
+
+  py::class_<Hello>(m, "Hello").def(py::init<>()).def("say", &Hello::say);
+}
---
+++ test-cases/std_map_with_only_one_item.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+        > File Name: std_map_with_only_one_item.cpp
+        > Author:
+        > Mail:
+        > Created Time: 2020年06月30日 星期二 11时10分57秒
+ ************************************************************************/
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+  std::map<int, int> m;
+  m.insert({100, 1000});
+
+  int ret = m.begin()->first;
+  std::cout << ret << std::endl;
+
+  std::map<int, int> m2;
+  std::cout << m2.begin()->first << std::endl;
+
+  return 0;
+}
