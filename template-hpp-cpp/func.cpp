76dff59d76ab652a0c0d5ee1bfa018998e631eee
hyl <1063766703@qq.com>
2020-07-05 10:57:44 +0800

    add: template-hpp-cpp

---
+++ template-hpp-cpp/func.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <typename T>
+T const& func(T const& v) {
+    return v;
+}
+
+// 显式生成模板实例
+// template int const& func(int const& v);
+
+// 或者有其他的函数调用了该模板也行
+int foo(int v) {;
+    func(v);
+    std::cout << "foo" << std::endl;
+}
